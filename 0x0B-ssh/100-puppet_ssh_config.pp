0fe5ec6f91a3385163ad457192a6a14e54e9fa8f
Lawal Abubakar <utility4all@gmail.com>
2023-05-05 08:40:32 -0700

    refactored puppet config

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,15 +1,26 @@
+# Define a file resource for the OpenSSH client configuration
 file { '/etc/ssh/ssh_config':
+  
+  # Ensure that the file exists and is a regular file
   ensure  => 'file',
+  
+  # Set the owner of the file to root
   owner   => 'root',
+  
+  # Set the group of the file to root
   group   => 'root',
+  
+  # Set the file permissions to 0644 (-rw-r--r--)
   mode    => '0644',
+  
+  # Set the contents of the file to the following configuration options
   content => "
     Host *
-        PasswordAuthentication no
-        IdentityFile ~/.ssh/school
-        SendEnv LANG LC_*
-        HashKnownHosts yes
-        GSSAPIAuthentication yes
-        GSSAPIDelegateCredentials no
+        PasswordAuthentication no           # Disable password authentication
+        IdentityFile ~/.ssh/school          # Specify the location of the identity file
+        SendEnv LANG LC_*                   # Send environment variables
+        HashKnownHosts yes                  # Enable hashing of known hosts
+        GSSAPIAuthentication yes            # Enable GSSAPI authentication
+        GSSAPIDelegateCredentials no        # Do not delegate credentials
   ",
 }
